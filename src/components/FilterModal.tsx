d4e77e7ac4ff4a68b05f1ccc32aa6f204495ec22
Ryan Slama <ryanmslama@gmail.com>
2024-12-23 15:29:31 -0500

    Print styling

--- src/components/FilterModal.tsx
+++ src/components/FilterModal.tsx
@@ -40,7 +40,7 @@ export function FilterModal({
   const btn = (
     <Button
       variant="ghost"
-      className="font-serif text-xl w-full mb-16 text-muted-foreground"
+      className="font-serif text-xl w-full mb-16 text-muted-foreground print:hidden"
     >
       <Funnel size={32} />
       Filter Drinks
